5a763313d9b84549012793a264fa10f013bf98f6
James Robinson <jamesrobinson@jamess-mbp-2.default>
2014-03-17 22:22:02 +0000

    changed protect_from_forgery with to null_session

--- app/models/location.rb
+++ app/models/location.rb
@@ -9,7 +9,7 @@ class Location < ActiveRecord::Base
 	#instead of letting users enter the long and lat themselves we'll have Geocoder do that by the address
 
 	geocoded_by :name
-	after_validation :geocode # this is not needed because i've already gotten the long and lat from the scrape
+	# after_validation :geocode # this is not needed because i've already gotten the long and lat from the scrape
 
 
 
